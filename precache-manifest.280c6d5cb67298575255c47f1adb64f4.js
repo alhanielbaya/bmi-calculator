bc0ac24c6262026d228ebc319dd92d73b21b3f13
AlStrom201 <keepostrom@gmail.com>
2019-10-23 14:16:34 +0800

    Updates

---
+++ precache-manifest.280c6d5cb67298575255c47f1adb64f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4d9966042b25da18331d60e5a8b718",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "9e6d81c7a0b1978f6e5c",
+    "url": "/bmi-calculator/static/css/2.eff14830.chunk.css"
+  },
+  {
+    "revision": "14dc1d4d6f751c9fa77e",
+    "url": "/bmi-calculator/static/css/main.43cc6633.chunk.css"
+  },
+  {
+    "revision": "9e6d81c7a0b1978f6e5c",
+    "url": "/bmi-calculator/static/js/2.62db18f1.chunk.js"
+  },
+  {
+    "revision": "14dc1d4d6f751c9fa77e",
+    "url": "/bmi-calculator/static/js/main.5414291b.chunk.js"
+  },
+  {
+    "revision": "1473c5d8e6df3d7e4c84",
+    "url": "/bmi-calculator/static/js/runtime-main.655f34e1.js"
+  }
+]);
